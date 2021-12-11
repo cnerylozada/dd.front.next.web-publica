67a18cb59b89e8559ad6e458d528e91140e6fe73
cnerylozada <cnery@dynamicdevs.io>
2021-12-11 17:09:56 -0500

    chore: in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+        danger: "var(--danger)",
+        green: "var(--green)",
+        turquoise: "var(--turquoise)",
+        blue: "var(--blue)",
+        purple: "var(--purple)",
+        lemon: "var(--lemon)",
+
+        text: "var(--text)",
+
+        bg1: "var(--bg1)",
+        bg2: "var(--bg2)",
+        bg3: "var(--bg3)",
+
+        gray1: "var(--gray1)",
+        gray2: "var(--gray2)",
+        gray3: "var(--gray3)",
+      },
+    },
   },
   variants: {
     extend: {},
