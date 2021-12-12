c004f18425e05845ca50a6e8fec110127b32700f
cnerylozada <cnery@dynamicdevs.io>
2021-12-12 17:01:14 -0500

    feat: staff section

--- components/home-staff.tsx
+++ components/home-staff.tsx
@@ -1,16 +1,51 @@
 import React from "react";
-import { DDText } from "@cnerylozada/dd.front.react.wp.library";
+import {
+  DDButton,
+  DDCardProfile,
+  DDText,
+} from "@cnerylozada/dd.front.react.wp.library";
+import { staff } from "../services";
 
 const HomeStaff = ({ people, lng }: { people: any; lng: string }) => {
   return (
-    <div className="px-7 pt-12 bg-bg2 text-text">
+    <div className="px-7 py-12 bg-bg2 text-text">
       <DDText size="headline" className="mb-6 text-primary">
         {people[`title_${lng}`]}
       </DDText>
       <DDText size="subHeadline" weight="light" className="mb-12">
-        {people[`description_${lng}`]}
+        {lng === "es"
+          ? "Nos dedicamos al desarrollo de software centrado en habilitar procesos de innovación apoyados en la tecnología. Nos mueve crear soluciones de impacto real."
+          : "We are dedicated to software development focused on enabling technology-supported innovation processes. We are moved to create solutions with real impact."}
       </DDText>
-      <div>.</div>
+      <div>
+        {staff.map((_, index) => (
+          <DDCardProfile
+            key={index}
+            name={_.name}
+            imgUrl={_.imageUrl}
+            linkedinUrl={_.link}
+            position={_.jobPosition}
+            className="mb-12"
+          />
+        ))}
+      </div>
+      <div>
+        <DDText
+          size="sectionHeadline"
+          weight="bold"
+          className="mb-6 text-primary uppercase"
+        >
+          {lng === "es"
+            ? "¿quieres ser parte del equipo?"
+            : "Want to be part of our team?"}
+        </DDText>
+        <DDText size="subHeadline" weight="light" className="mb-12">
+          {people[`description_${lng}`]}
+        </DDText>
+        <DDButton onClick={() => console.log("xxx")} className="block mx-auto">
+          {people[`bottom_title_${lng}`]}
+        </DDButton>
+      </div>
     </div>
   );
 };
--- services/home-sections.ts
+++ services/home-sections.ts
@@ -4,3 +4,30 @@ const url = "https://strapi.dynamicdevs.io";
 export const getHomeSections = () => {
   return axios.get(`${url}/home`).then(({ data }) => data);
 };
+
+export const staff = [
+  {
+    id: 1,
+    name: "Jonathan González",
+    jobPosition: "Chief Executive Officer",
+    imageUrl:
+      "https://dynamicdevscommon.blob.core.windows.net/web/jonathan-gozales.png",
+    link: "https://www.linkedin.com/in/jongon/",
+  },
+  {
+    id: 2,
+    name: "César Contreras",
+    jobPosition: "Chief Technology Officer",
+    imageUrl:
+      "https://dynamicdevscommon.blob.core.windows.net/web/cesar-contreras.png",
+    link: "https://www.linkedin.com/in/c1sar/",
+  },
+  {
+    id: 3,
+    name: "Marlis Mejías",
+    jobPosition: "Chief Marketing Officer",
+    imageUrl:
+      "https://dynamicdevscommon.blob.core.windows.net/web/marlis-mejias.png",
+    link: "https://www.linkedin.com/in/marlismejias/",
+  },
+];
