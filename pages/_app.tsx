e31115edf0333b4ede2d28ff4c45377e2eb61e24
cnerylozada <cnery@dynamicdevs.io>
2021-12-22 16:27:52 -0500

    feat: dd-lng and dd-theme storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,26 @@
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import { DDTheme } from "@cnerylozada/dd.front.react.wp.library";
-import { StoreProvider, reducer, initialState, useStore } from "../store";
+import {
+  StoreProvider,
+  reducer,
+  initialState,
+  useStore,
+  setTheme,
+  setLanguage,
+} from "../store";
 import { Navbar } from "../components";
-import { languages } from "../utils";
 import "../styles/globals.css";
 
 const WrapperApp = ({ children }: { children: any }) => {
-  const [store] = useStore();
+  const [store, dispatch] = useStore();
+
+  useEffect(() => {
+    const ddLng = localStorage.getItem("dd-lng");
+    const ddTheme = localStorage.getItem("dd-theme");
+    if (!!ddLng) dispatch(setLanguage(ddLng));
+    if (!!ddTheme) dispatch(setTheme(ddTheme === "dark"));
+  }, []);
 
   return (
     <div className={`relative ${store.ddIsDarkMode ? "darkPalette" : ""}`}>
@@ -19,19 +32,12 @@ const WrapperApp = ({ children }: { children: any }) => {
   );
 };
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    localStorage.setItem("dd-lng", languages.spanish);
-    localStorage.setItem("dd-theme", "light");
-  }, []);
-
-  return (
-    <StoreProvider initialState={initialState} reducer={reducer}>
-      <WrapperApp>
-        <Component {...pageProps} />
-      </WrapperApp>
-    </StoreProvider>
-  );
-};
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <StoreProvider initialState={initialState} reducer={reducer}>
+    <WrapperApp>
+      <Component {...pageProps} />
+    </WrapperApp>
+  </StoreProvider>
+);
 
 export default MyApp;
