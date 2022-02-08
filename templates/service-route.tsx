22508e4e45199252f4c6362c5b2696a439d6f52a
cnerylozada <cnery@dynamicdevs.io>
2022-02-07 19:54:01 -0500

    feat: max-width services templates

--- templates/service-route.tsx
+++ templates/service-route.tsx
@@ -14,50 +14,52 @@ export const ServiceRoute = ({
   const { Service, SubService } = data;
 
   return (
-    <div className={`py-12 lg:py-38 ${className}`}>
-      <div className="mb-14 lg:mb-16 px-7 lg:px-45">
-        <DDText size="headline" weight="bold" className={`mb-5`}>
-          {Service[`title_${lng}`]}
-        </DDText>
-        <DDText size="subHeadline" weight="light" className="text-text">
-          {Service[`description_${lng}`]}
-        </DDText>
-      </div>
+    <div className={`${className}`}>
+      <div className={`mx-auto py-12 lg:py-38 max-w-[1440px]`}>
+        <div className="mb-14 lg:mb-16 px-7 lg:px-45">
+          <DDText size="headline" weight="bold" className={`mb-5`}>
+            {Service[`title_${lng}`]}
+          </DDText>
+          <DDText size="subHeadline" weight="light" className="text-text">
+            {Service[`description_${lng}`]}
+          </DDText>
+        </div>
 
-      <div className="mx-auto px-7 lg:px-0 w-route lg:w-222 flex flex-col">
-        {SubService.map((_: any, index: number) => (
-          <div
-            key={index}
-            className={`relative mb-25 last:mb-0 lg:mb-0 lg:pt-5 ${
-              !!((index + 1) % 2) ? "" : "self-end"
-            } flex flex-col items-center w-62 lg:w-80 h-62 lg:h-80 ${borderStyles}
+        <div className="mx-auto px-7 lg:px-0 w-route lg:w-222 flex flex-col">
+          {SubService.map((_: any, index: number) => (
+            <div
+              key={index}
+              className={`relative mb-25 last:mb-0 lg:mb-0 lg:pt-5 ${
+                !!((index + 1) % 2) ? "" : "self-end"
+              } flex flex-col items-center w-62 lg:w-80 h-62 lg:h-80 ${borderStyles}
               rounded-full border border-dashed text-center`}
-          >
-            {!!((index + 1) % 2) && index !== 0 && (
-              <div
-                className={`absolute top-0 lg:top-21 left-1/2 lg:left-74 ${borderStyles}
-                w-28 lg:w-84 origin-top-left -rotate-60 lg:-rotate-32 border-t border-dashed`}
-              ></div>
-            )}
-            <DDIcon name={_.icon} size="xl" />
-            <DDText
-              size="subHeadline"
-              weight="bold"
-              className="my-2 lg:my-5 text-text"
             >
-              {_.Service[`title_${lng}`]}
-            </DDText>
-            <DDText weight="light" className="w-4/5 text-text">
-              {_.Service[`description_${lng}`]}
-            </DDText>
-            {!!((index + 1) % 2) && index !== SubService.length - 1 && (
-              <div
-                className={`absolute top-full lg:top-auto lg:bottom-18 left-1/2 lg:left-72 ${borderStyles}
+              {!!((index + 1) % 2) && index !== 0 && (
+                <div
+                  className={`absolute top-0 lg:top-21 left-1/2 lg:left-74 ${borderStyles}
+                w-28 lg:w-84 origin-top-left -rotate-60 lg:-rotate-32 border-t border-dashed`}
+                ></div>
+              )}
+              <DDIcon name={_.icon} size="xl" />
+              <DDText
+                size="subHeadline"
+                weight="bold"
+                className="my-2 lg:my-5 text-text"
+              >
+                {_.Service[`title_${lng}`]}
+              </DDText>
+              <DDText weight="light" className="w-4/5 text-text">
+                {_.Service[`description_${lng}`]}
+              </DDText>
+              {!!((index + 1) % 2) && index !== SubService.length - 1 && (
+                <div
+                  className={`absolute top-full lg:top-auto lg:bottom-18 left-1/2 lg:left-72 ${borderStyles}
                   w-28 lg:w-84 origin-top-left rotate-60 lg:rotate-32 border-t border-dashed`}
-              ></div>
-            )}
-          </div>
-        ))}
+                ></div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- templates/service-title-icon.tsx
+++ templates/service-title-icon.tsx
@@ -10,40 +10,42 @@ export const ServiceTitleIcon = ({
   className: string;
 }) => {
   return (
-    <div
-      className={`px-7 lg:px-45 h-screen flex flex-col justify-center
-      ${className}`}
-    >
-      {!!data.Service && (
-        <DDText size="headline" weight="bold" className={`mb-6 lg:mb-14`}>
-          {data.Service[`title_${lng}`]}
-        </DDText>
-      )}
-      {!data.Service && (
-        <DDText size="headline" weight="bold" className={`mb-6 lg:mb-10`}>
-          {data[`title_${lng}`]}
-        </DDText>
-      )}
-
-      <div className="flex flex-col lg:flex-row items-center">
-        {!!data.icon && (
-          <DDIcon
-            name={data.icon}
-            size="graphic"
-            className={`block mb-10 lg:mb-0 lg:mr-24`}
-          />
-        )}
-
+    <div className={`${className}`}>
+      <div
+        className={`px-7 lg:px-45 mx-auto max-w-[1440px] h-screen 
+        flex flex-col justify-center`}
+      >
         {!!data.Service && (
-          <DDText size="subHeadline" weight="light" className="text-text">
-            {data.Service[`description_${lng}`]}
+          <DDText size="headline" weight="bold" className={`mb-6 lg:mb-14`}>
+            {data.Service[`title_${lng}`]}
           </DDText>
         )}
         {!data.Service && (
-          <DDText size="subHeadline" weight="light" className="text-text">
-            {data[`description_${lng}`]}
+          <DDText size="headline" weight="bold" className={`mb-6 lg:mb-10`}>
+            {data[`title_${lng}`]}
           </DDText>
         )}
+
+        <div className="flex flex-col lg:flex-row items-center">
+          {!!data.icon && (
+            <DDIcon
+              name={data.icon}
+              size="graphic"
+              className={`block mb-10 lg:mb-0 lg:mr-24`}
+            />
+          )}
+
+          {!!data.Service && (
+            <DDText size="subHeadline" weight="light" className="text-text">
+              {data.Service[`description_${lng}`]}
+            </DDText>
+          )}
+          {!data.Service && (
+            <DDText size="subHeadline" weight="light" className="text-text">
+              {data[`description_${lng}`]}
+            </DDText>
+          )}
+        </div>
       </div>
     </div>
   );
